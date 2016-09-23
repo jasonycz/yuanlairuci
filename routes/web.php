8dca618d233879a2e1696c5b077451dd4018c282
jasonyang <jsonyang@163.com>
2016-09-23 13:55:00 +0800

    update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'hello';
+});
+Route::any('/wechat', 'WechatController@serve');
+
+Route::group(['middleware' => ['web']], function(){//web 是middleware??
+	Route::get('/image','MaterialController@image');
+	Route::get('/audio','MaterialController@audio ');
+	Route::get('/materialList','MaterialController@materialList');
+	Route::get('/test','TestController@show');
+	Route::get('/message','MaterialController@message');
+	Route::get('/menu/add','MenuController@add');
+	Route::get('/menu/all','MenuController@all');
+	Route::get('/menu/delete','MenuController@delete');
+
+
+	Route::get('/qrcode/create','QrCodeController@create');
+});
+
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+
+        dd($user);
+        
+    });
 });
